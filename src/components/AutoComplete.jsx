785fa7e0d5220a77a17b0a0498fdf580acb94a66
joeunhye <masimel33@gmail.com>
2023-07-12 23:55:38 +0900

    pokemon book search

--- src/components/AutoComplete.jsx
+++ src/components/AutoComplete.jsx
@@ -5,7 +5,7 @@ const AutoComplete = ({ allpokemons, setDisplayedPokemons }) => {
 
 	const filterNames = input => {
 		const value = input.toLowerCase();
-		return value ? allpokemons.filter(pokemon => pokemon.name.includes(value)) : [];
+		return value ? allpokemons.filter(pokemon => pokemon?.name.includes(value)) : [];
 	};
 	const handleSubmit = e => {
 		e.preventDefault();
@@ -15,7 +15,7 @@ const AutoComplete = ({ allpokemons, setDisplayedPokemons }) => {
 	};
 	return (
 		<div className="relative z-50">
-			<form onSubmit={handleSubmit} action="" className="relative flex justify-center items-center w-[20.5rem] h-6 rounded-lg m-auto">
+			<form onSubmit={handleSubmit} className="relative flex justify-center items-center w-[20.5rem] h-6 rounded-lg m-auto">
 				<input type="text" className="text-xs w-[20.5rem] h-6 px-2 py-1 bg-slate-500 rounded-lg text-gray-100 text-center" onChange={e => setSearchTerm(e.target.value)} value={searchTerm} />
 				<button className="text-xs text-slate-300 w-[2.5rem] h-6 px-2 py-1 rounded-lg text-center absolute right-0 hover:bg-slate-700">검색</button>
 			</form>
