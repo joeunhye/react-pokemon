ae9f0d79c1d9928c36fe2fa80b5c6735bcfdb64f
joeunhye <masimel33@gmail.com>
2023-08-08 23:20:22 +0900

    react study

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+	apiKey: "AIzaSyBv2lJUUbSPsgzvd-LToFbkAeW7iRxk1EE",
+	authDomain: "react-pokemon-d1936.firebaseapp.com",
+	projectId: "react-pokemon-d1936",
+	storageBucket: "react-pokemon-d1936.appspot.com",
+	messagingSenderId: "231296781300",
+	appId: "1:231296781300:web:78258e13ef1776d02907a1",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
